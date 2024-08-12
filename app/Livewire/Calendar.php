a6d32c2a622f3acf6118cdf30feb2e0beddd697b
Clement Couturier <ccouturier@b2pweb.com>
2024-08-12 11:25:29 +0200

    [+] Calendar récupère les functions de EventService et EventComponent

--- app/Livewire/Calendar.php
+++ app/Livewire/Calendar.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Events;
 use Livewire\Component;
 
 class Calendar extends Component
@@ -19,6 +20,72 @@ class Calendar extends Component
         $this->user = auth()->user();
         $this->team = $this->user->currentTeam;
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    /// Events ///
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+    public function create($data)
+    {
+        $event = new Events($data);
+        $event->save();
+
+        return $event;
+    }
+
+    public function update($id, $data)
+    {
+
+        $event = Events::find($id);
+        $event->fill($data);
+        $event->save();
+
+        return $event;
+    }
+
+    public function refetchEvents($selectedUsers = null)
+    {
+        if (! $selectedUsers) {
+            $selectedUsers = [0];
+        }
+
+        $allUsersEvents = [];
+
+        foreach ($selectedUsers as $selectedUser) {
+
+            $eventQuery = Events::query();
+            $eventQuery->where('user_id', $selectedUser);
+            $events = $eventQuery->get();
+            $existingsEventsIDs = [];
+            
+            if ($events) {
+
+                foreach ($events as $event) {
+
+                    if (! (int) $event['is_all_day']) {
+                        $event['allDay'] = false;
+                        $event['start'] = $event['start'];
+                        $event['end'] = $event['end'];
+                        $event['endDay'] = $event['end'];
+                        $event['startDay'] = $event['start'];
+                    } else {
+                        $event['allDay'] = true;
+                        $event['endDay'] = $event['end'];
+                        $event['end'] = $event['end'];
+                        $event['startDay'] = $event['start'];
+                    }
+                    array_push($allUsersEvents, $event);
+                    array_push($existingsEventsIDs, $event->event_id);
+                }
+            }
+        }
+
+        return json_encode($allUsersEvents);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    /// Render ///
+    ////////////////////////////////////////////////////////////////////////////////////////
 
     public function render()
     {
